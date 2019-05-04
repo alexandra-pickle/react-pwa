17806a6013ef898fbeb4250ab50bc3ee70cf05a4
Alexandra Dyshlyuk <alexandradyshlyuk@Alexandras-MacBook-Pro.local>
2019-05-04 10:09:43 -0500

    Updates

---
+++ precache-manifest.7ff33f5c05e0f1416dc5d93807d955a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d5b8aa84372d6ea9ff6c98b7cb71914",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ce560ac41a36e5db1c7",
+    "url": "/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "fadbb0705bdd11d4fb8f",
+    "url": "/static/js/2.5d907561.chunk.js"
+  },
+  {
+    "revision": "9ce560ac41a36e5db1c7",
+    "url": "/static/js/main.4f3e1fab.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
